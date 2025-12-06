5166f596767cf5b5919181d4541cbdee9385148c
Luis-Gustavo-LG <nickgrs07@gmail.com>
2025-12-06 18:52:17 -0300

    feat: add get-user-by-id on user-routes

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { CreateUserController } from "../controllers/create-user.js";
+import { GetUserByIdController } from "../controllers/get-user-by-id.js";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
+const getUserByIdController = new GetUserByIdController();
 
 router.post('/', createUserController.execute);
+router.get('/:id', getUserByIdController.execute);
 
 export default router;
 
