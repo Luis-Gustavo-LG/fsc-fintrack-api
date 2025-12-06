f13cbd64ad5fdbb2e7df66ecbb93e80879e3172f
Luis-Gustavo-LG <nickgrs07@gmail.com>
2025-12-06 18:50:27 -0300

    feat: add success helper

--- src/controllers/helpers.js
+++ src/controllers/helpers.js
@@ -5,3 +5,4 @@ export const sendResponse = (res, status, body) => {
 export const badRequest  = (res, body) => sendResponse(res, 400, body);
 export const created     = (res, body) => sendResponse(res, 201, body);
 export const serverError = (res, body) => sendResponse(res, 500, body);
+export const success     = (res, body) => sendResponse(res, 200, body);
