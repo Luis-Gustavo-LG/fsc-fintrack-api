69190d794c570574d7cd4d0a4c27400548769378
Luis-Gustavo-LG <nickgrs07@gmail.com>
2025-12-07 09:55:03 -0300

    feat: add validation if user is different from user id on UpdateUserUseCase

--- src/use-cases/update-user.js
+++ src/use-cases/update-user.js
@@ -1,16 +1,17 @@
 import bcrypt from "bcrypt";
-import { EmailAlreadyInUseError } from "../controllers/errors/user";
+import { EmailAlreadyInUseError } from "../controllers/errors/user.js";
 import { PostgresGetUserByEmailRepository } from "../repositories/postgres/get-user-by-email.js";
 import { PostgresUpdateUserRepository } from "../repositories/postgres/update.user.js";
 
 export class UpdateUserUseCase {
     async execute(userId, updateUserParams) {
+
         if(updateUserParams.email) {
             const postgresGetUserByEmailRepository = new PostgresGetUserByEmailRepository();
 
             const user = await postgresGetUserByEmailRepository.execute(updateUserParams.email);
 
-            if(user) {
+            if(user && user.id !== userId) {
                 throw new EmailAlreadyInUseError(updateUserParams.email);
             }
         }
