4cdb7a4c217aedb9c04c5ffb29be1ab4ed626082
Luis-Gustavo-LG <nickgrs07@gmail.com>
2025-12-07 08:56:57 -0300

    feat: add UpdateUserUseCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,32 @@
+import bcrypt from "bcrypt";
+import { EmailAlreadyInUseError } from "../controllers/errors/user";
+import { PostgresGetUserByEmailRepository } from "../repositories/postgres/get-user-by-email.js";
+import { PostgresUpdateUserRepository } from "../repositories/postgres/update.user.js";
+
+export class UpdateUserUseCase {
+    async execute(userId, updateUserParams) {
+        if(updateUserParams.email) {
+            const postgresGetUserByEmailRepository = new PostgresGetUserByEmailRepository();
+
+            const user = await postgresGetUserByEmailRepository.execute(updateUserParams.email);
+
+            if(user) {
+                throw new EmailAlreadyInUseError(updateUserParams.email);
+            }
+        }
+
+        const user = {
+            ...updateUserParams
+        }
+
+        if(updateUserParams.password) {
+            const passwordHash = await bcrypt.hash(updateUserParams.password, 10);
+            user.password = passwordHash;
+        }
+
+        const postgresUpdateUserRepository = new PostgresUpdateUserRepository();
+        const updatedUser = await postgresUpdateUserRepository.execute(userId, user);
+
+        return updatedUser;
+    }
+}
