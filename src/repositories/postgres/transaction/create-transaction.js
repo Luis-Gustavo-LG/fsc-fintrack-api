21ff5c7f55f31d63d7de79567d61a4a86601907e
Luis-Gustavo-LG <nickgrs07@gmail.com>
2025-12-09 09:46:48 -0300

    feat: add PostgresCreateTransactionRepository

--- src/repositories/postgres/transaction/create-transaction.js
+++ src/repositories/postgres/transaction/create-transaction.js
@@ -3,9 +3,16 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export class PostgresCreateTransactionRepository {
-    async execute(transaction) {
+    async execute(createTransactionParams) {
         const result = await prisma.transaction.create({
-            data: transaction
+            data: {
+                name: createTransactionParams.name,
+                type: createTransactionParams.type,
+                amountInCents: createTransactionParams.amountInCents,
+                description: createTransactionParams.description,
+                date: createTransactionParams.date,
+                userId: createTransactionParams.userId,
+            }
         })
 
         return result;
