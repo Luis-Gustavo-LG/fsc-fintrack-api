35347c62c95c0c4a68e47135adc380651be32292
Luis-Gustavo-LG <nickgrs07@gmail.com>
2025-12-10 23:33:23 -0300

    feat: add makeGetTransactionsByIdController in transaction routes

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
-import { makeCreateTransactionController } from "../factories/controllers/transaction.js";
+import { makeCreateTransactionController, makeGetTransactionsByUserIdController } from "../factories/controllers/transaction.js";
 
 export class TransactionRoutes {
     constructor() {
         this.router = Router();
-
         this.setupRoutes();
     }
 
     setupRoutes() {
         this.router.post('/:userId/transaction', makeCreateTransactionController().execute);
+        this.router.get('/:userId/transaction', makeGetTransactionsByUserIdController().execute);
     }
 
     getRouter() {
