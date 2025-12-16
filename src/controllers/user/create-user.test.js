53880d7d9686c02f7672299730a2fed39a2a5a83
Luis-Gustavo-LG <nickgrs07@gmail.com>
2025-12-16 00:33:23 -0300

    test: Create user with blank firstName done successfully

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -191,3 +191,29 @@ describe("Create user with less than 6 characters password", () => {
         expect(httpResponse.json).toHaveBeenCalledWith({ message: "Password must have at least 6 characters" });
     })
 })
+
+describe("Create user with blank firstName", () => {
+    it("should return a bad request error", async () => {
+        //arrange
+        const createUserUseCaseStub = new CreateUserUseCaseStub();
+        const createUserController = new CreateUserController(createUserUseCaseStub);
+
+        const httpRequest = {
+            body: {
+                firstName: "       ",
+                lastName: "Doe",
+                email: "john.doe@example.com",
+                password: "123456",
+            }
+        };
+
+        const httpResponse = makeResponse();
+
+        //act
+        await createUserController.execute(httpRequest, httpResponse);
+
+        //assert      
+        expect(httpResponse.status).toHaveBeenCalledWith(400);
+        expect(httpResponse.json).toHaveBeenCalledWith({ message: "First name must have at least 2 characters" });
+    })
+})
