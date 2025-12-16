55d8f65e08ec6e38e6ae9f42b15df769e3b4fb63
Luis-Gustavo-LG <nickgrs07@gmail.com>
2025-12-16 00:24:19 -0300

    test: Create User Controller, Create user without firstName, Create user without lastName and Create user without email tests done successfuly

---
+++ src/controllers/user/create-user.test.js
@@ -0,0 +1,116 @@
+import { CreateUserController } from "./create-user.js"
+
+class CreateUserUseCaseStub {
+    execute(user) {
+        return user
+    }
+}
+
+const makeResponse = () => {
+    const res = {};
+    res.status = jest.fn().mockReturnValue(res);
+    res.json = jest.fn().mockReturnValue(res);
+    return res;
+};
+
+
+describe("Create User Controller", () => {
+    it("should create a user", async () => {
+        //arrange
+        const createUserUseCaseStub = new CreateUserUseCaseStub();
+        const createUserController = new CreateUserController(createUserUseCaseStub);
+
+        const httpRequest = {
+            body: {
+                firstName: "John",
+                lastName: "Doe",
+                email: "john.doe@example.com",
+                password: "123456",
+            }
+        };
+
+        const httpResponse = makeResponse();
+
+        //act
+        await createUserController.execute(httpRequest, httpResponse);
+
+        //assert      
+        expect(httpResponse.status).toHaveBeenCalledWith(201);
+        expect(httpResponse.json).toHaveBeenCalledWith(httpRequest.body);
+    })
+})
+
+describe("Create user without firstName", () => {
+    it("should return a bad request error", async () => {
+        //arrange
+        const createUserUseCaseStub = new CreateUserUseCaseStub();
+        const createUserController = new CreateUserController(createUserUseCaseStub);
+
+        const httpRequest = {
+            body: {
+                lastName: "Doe",
+                email: "john.doe@example.com",
+                password: "123456",
+            }
+        };
+
+        const httpResponse = makeResponse();
+
+        //act
+        await createUserController.execute(httpRequest, httpResponse);
+
+        //assert      
+        expect(httpResponse.status).toHaveBeenCalledWith(400);
+        expect(httpResponse.json).toHaveBeenCalledWith({ message: "First name is required" });
+    })
+})
+
+describe("Create user without lastName", () => {
+    it("should return a bad request error", async () => {
+        //arrange
+        const createUserUseCaseStub = new CreateUserUseCaseStub();
+        const createUserController = new CreateUserController(createUserUseCaseStub);
+
+        const httpRequest = {
+            body: {
+                firstName: "John",
+                email: "john.doe@example.com",
+                password: "123456",
+            }
+        };
+
+        const httpResponse = makeResponse();
+
+        //act
+        await createUserController.execute(httpRequest, httpResponse);
+
+        //assert      
+        expect(httpResponse.status).toHaveBeenCalledWith(400);
+        expect(httpResponse.json).toHaveBeenCalledWith({ message: "Last name is required" });
+    })
+})
+
+describe("Create user without email", () => {
+    it("should return a bad request error", async () => {
+        //arrange
+        const createUserUseCaseStub = new CreateUserUseCaseStub();
+        const createUserController = new CreateUserController(createUserUseCaseStub);
+
+        const httpRequest = {
+            body: {
+                firstName: "John",
+                lastName: "Doe",
+                password: "123456",
+            }
+        };
+
+        const httpResponse = makeResponse();
+
+        //act
+        await createUserController.execute(httpRequest, httpResponse);
+
+        //assert      
+        expect(httpResponse.status).toHaveBeenCalledWith(400);
+        expect(httpResponse.json).toHaveBeenCalledWith({ message: "Invalid email" });
+    })
+})
